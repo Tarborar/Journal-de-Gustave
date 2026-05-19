a337ffbc0f61ffd98e41d3d402e64272dff36de9
Tarborar <mathieuseurre84@gmail.com>
2026-05-19 19:35:53 +0200

    Page journal carre

--- src/pages/Journaux.jsx
+++ src/pages/Journaux.jsx
@@ -1,8 +1,32 @@
+import '../styles/pages/Journaux.scss';
+import backgroundJournal from '../assets/backgroundData.png'; 
+import { journauxList } from '../datas/journauxList';
+import journalButton from '../assets/journalButton2.svg';
+
 function Journaux(){
     return(
-        <>
-        
-        </>
+        <div className='horizontal'>
+            <div className='vertical'>
+                <p className='journal__paragraph'>Complétez tous les journaux</p>
+                <div className='relative'>
+                    <img src={backgroundJournal} className='backgroundJournal' alt="" />
+                    <div className='journaux__select absolute'>
+                        {journauxList.map((j) => (
+                            <div className='relative'>
+                                <img src={journalButton} className='journalButton' alt="" />
+                                <div className='journaux__carre bebasNueue mainColor'>{j.numero}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
     )
 }
 
