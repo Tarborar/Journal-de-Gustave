ebbfbe84f7224f339972b49f4f9379ba7c1c4189
Tarborar <mathieuseurre84@gmail.com>
2026-06-01 14:00:40 +0200

    Responsive smallDesktop

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,5 @@
 import searchInput from '../assets/searchInput.svg';
+import smallSearchInput from '../assets/smallSearchInput.svg';
 import searchIcon from '../assets/searchIcon.svg';
 
 import '../styles/components/Search.scss'
@@ -11,7 +12,10 @@ function Search({ setInputValue }){
     return(
         <div className='relative search'>
             <input type="search" className='search__input absolute' onInput={search}/>
-            <img src={searchInput} className='search__image' alt="" />
+            <picture>
+                <source media="(max-width: 1440px)" srcSet={smallSearchInput} />
+                <img src={searchInput} className='search__image' alt="" />
+            </picture>
             <img src={searchIcon} className='search__icon' alt="" />
         </div>
     )
